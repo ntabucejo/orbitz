63a5bbbae3b964a97403f993861de2c5f8231d7c
ntabucejo <ntabucejo@gmail.com>
2022-05-22 02:29:34 +0800

    Add planets data

---
+++ library/data/planets.ts
@@ -0,0 +1,46 @@
+import cuid from 'cuid'
+import IPlanet from '../schemas/interfaces/planet'
+
+class CreatePlanet implements IPlanet {
+  id: string
+  name: string
+  image: string
+  diameter: number
+  orbitRadius: number
+  constructor(id: string, name: string, image: string, diameter: number, orbitRadius: number) {
+    this.id = id
+    this.name = name
+    this.image = image
+    this.diameter = diameter
+    this.orbitRadius = orbitRadius
+  }
+}
+
+const planets: IPlanet[] = [
+  {
+    ...new CreatePlanet(cuid(), 'mercury', '/images/mercury.png', 6, 14),
+  },
+  {
+    ...new CreatePlanet(cuid(), 'venus', '/images/venus.png', 9, 18),
+  },
+  {
+    ...new CreatePlanet(cuid(), 'earth', '/images/earth.png', 8, 24),
+  },
+  {
+    ...new CreatePlanet(cuid(), 'mars', '/images/mars.png', 5, 30),
+  },
+  {
+    ...new CreatePlanet(cuid(), 'jupiter', '/images/jupiter.png', 13, 48),
+  },
+  {
+    ...new CreatePlanet(cuid(), 'saturn', '/images/saturn.png', 8, 66),
+  },
+  {
+    ...new CreatePlanet(cuid(), 'uranus', '/images/uranus.png', 3, 80),
+  },
+  {
+    ...new CreatePlanet(cuid(), 'neptune', '/images/neptune.png', 3, 90),
+  },
+]
+
+export default planets
