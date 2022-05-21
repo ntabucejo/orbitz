cb349abcc35cf726c7780fa37d92038bf9a22e1b
ntabucejo <ntabucejo@gmail.com>
2022-05-22 02:30:17 +0800

    Add next head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '../components/layout'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Orbitz</title>
+        <meta name="description" content="Orbitz is a solar system visualizer relative to earth's orbit. " />
+        <link rel="icon" href="/images/logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
